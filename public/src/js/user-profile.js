ecdc819867f3d2e6caeb646a2f9a7e747eefb73d
Harrison Lingren <harrisonlingren@gmail.com>
2017-12-22 09:41:19 -0500

    profile loads recipes, buildRecipeCard to global

--- public/src/js/user-profile.js
+++ public/src/js/user-profile.js
@@ -20,66 +20,7 @@ function loadUserRecipes(userId) {
   $.getJSON(APIurl + "recipe/all?user=" + userId).then(recipes => {
     // build recipes and append to page
     recipes.forEach(recipe => {
-      // put cards here
-      let recipeContainer = $(".recipe-container");
-
-      recipe.image = recipe.image || "http://via.placeholder.com/400x400";
-      recipe.description = recipe.description || "recipe.description";
-      let recipeImg = $("<div></div>")
-        .addClass("card-image-overflow waves-effect waves-block waves-light")
-        .append(
-          $("<img />")
-            .addClass("activator")
-            .attr("src", recipe.image)
-        );
-
-      // card title
-      let recipeTitle = $("<span></span>")
-        .addClass("card-title activator grey-text text-darken-4")
-        .html(recipe.name + '<i class="material-icons right">more_vert</i>');
-
-      // completed card content
-      let recipeContent = $("<div></div>")
-        .addClass("card-content")
-        .append(recipeTitle);
-
-      // card action
-      let recipeLink = $("<div></div>")
-        .addClass("card-action")
-        .append(
-          $("<a></a>")
-            .attr("href", "recipes/" + recipe._id)
-            .text("Open recipe")
-        );
-
-      // card summary
-      let recipeSummary = $("<p></p>").text(recipe.description);
-
-      // card reveal content
-      let recipeReveal = $("<div></div>")
-        .addClass("card-reveal")
-        .append(
-          $("<span></span>")
-            .addClass("card-title activator grey-text text-darken-4")
-            .html(recipe.name + '<i class="material-icons right">close</i>')
-        )
-        .append(recipeSummary);
-
-      // completed card
-      let recipeCard = $("<div></div>")
-        .addClass("card sticky-action")
-        .append(recipeImg)
-        .append(recipeContent)
-        .append(recipeLink)
-        .append(recipeReveal);
-
-      // create col for layout
-      $("<div></div>")
-        .addClass("col s12 l6")
-        .append(recipeCard)
-
-        // add to the container
-        .appendTo(recipeContainer);
+      buildRecipeCard(recipe, ".recipe-container");
     });
   });
 }
